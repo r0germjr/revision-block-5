7396b5aefddb702a263c44d0367c181bdfa68403
r0ger <rogeriomaiajr13@gmail.com>
2021-06-06 21:26:26 -0300

    Resolve o exercicio 3

--- exercises-5.3/exercise/script.js
+++ exercises-5.3/exercise/script.js
@@ -66,5 +66,28 @@ function createButtonHoliday(buttonName) {
     newButton.innerHTML = buttonName;
     buttonContainner.appendChild(newButton);
 }
-
 createButtonHoliday('Feriados');
+
+// 3- Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+//É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
+
+function showHolidays() {
+    let getHolidayButton = document.querySelector('#btn-holiday');
+    let getHolidays = document.querySelectorAll('.holiday');
+    let backgroundColor = 'rgb(238,238,238)';
+    let setNewColor = 'yellow';
+
+    getHolidayButton.addEventListener('click', function() {
+        for (let index = 0; index < getHolidays.length; index += 1) {
+            if (getHolidays[index].style.backgroundColor === setNewColor) {
+                getHolidays[index].style.backgroundColor = backgroundColor;
+            } else {
+                getHolidays[index].style.backgroundColor = setNewColor;
+            }
+        }
+    })
+};
+
+showHolidays();
+
+
